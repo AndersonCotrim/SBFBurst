c341eee0d3d84268852cc4b40f2715ea815d669f
Anderson <ander.cotrim@gmail.com>
2024-09-03 11:39:01 -0300

    remove git lfs, update readme and installer script

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 pip install torch==2.0.1 torchvision==0.15.2 torchaudio==2.0.2 --index-url https://download.pytorch.org/whl/cu118
 pip install mmcv==2.0.0 -f https://download.openmmlab.com/mmcv/dist/cu118/torch2.0/index.html
-pip install cupy-cuda11x==12.3.0
+conda install -c conda-forge cupy==12.3.0 cuda-version=11.8
 pip install -r requirements.txt
